5bc63a0f84bdfb037d4fc58c95653db9c479b358
andriykhomik <andriykhomik1992@gmail.com>
2021-11-29 20:10:24 +0200

    finished by my way

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 const images = document.querySelectorAll('#images img');
-const imgContainer = document.querySelector('#images');
 const imageContainer = document.querySelector('.image-container');
 const leftButton = document.querySelector('#left');
 const rightButton = document.querySelector('#right');
 
-let image = 0;
+let imagI = 0;
 
 function moveImagesRight(){
-    image++;
-    if (image === images.length) image = 0;
-    imageContainer.style.transform = `translateX(${-image * 500}px)`
+    imagI++;
+    if (imagI === images.length) imagI = 0;
+    imageContainer.style.transform = `translateX(${-imagI * 500}px)`
 }
 setInterval(moveImagesRight, 2000);
 
@@ -18,19 +17,7 @@ rightButton.addEventListener('click', ()=>{
 })
 
 leftButton.addEventListener('click', ()=> {
-    image--;
-    if (image < 0) image = images.length - 1;
-    imageContainer.innerHTML = '';
-    imageContainer.appendChild(images[image]);
+    imagI--;
+    if (imagI < 0) imagI = images.length - 1;
+    imageContainer.style.transform = `translateX(${-imagI * 500}px)`
 })
-
-
-// images.forEach(imag => {
-//     imageContainer.innerHTML = '';
-//     imageContainer.appendChild(images[1]);
-// })
-
-
-
-
-
