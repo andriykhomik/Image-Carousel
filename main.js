4e67471ec1fff7515872a4644a2d0a2321b0b7a0
andriykhomik <andriykhomik1992@gmail.com>
2021-11-29 21:33:51 +0200

    improved

--- main.js
+++ main.js
@@ -5,19 +5,29 @@ const rightButton = document.querySelector('#right');
 
 let imagI = 0;
 
-function moveImagesRight(){
-    imagI++;
-    if (imagI === images.length) imagI = 0;
-    imageContainer.style.transform = `translateX(${-imagI * 500}px)`
-}
-setInterval(moveImagesRight, 2000);
+let interval = setInterval(run, 2000);
 
 rightButton.addEventListener('click', ()=>{
-    moveImagesRight();
+    imagI++;
+    clearInterval(interval);
+    interval = setInterval(run, 2000);
+    changeImg();
 })
 
 leftButton.addEventListener('click', ()=> {
     imagI--;
+    clearInterval(interval);
+    interval = setInterval(run, 2000);
+    changeImg();
+})
+
+function run(){
+    imagI++
+    changeImg();
+}
+
+function changeImg(){
+    if (imagI > images.length - 1) imagI = 0;
     if (imagI < 0) imagI = images.length - 1;
     imageContainer.style.transform = `translateX(${-imagI * 500}px)`
-})
+}
